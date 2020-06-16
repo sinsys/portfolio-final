6fde01b74eb0e7216ace5629b44c677ace20f340
sinsys <nicholas.hazel@gmail.com>
2020-06-16 10:35:25 -0700

    Initial files created. Basic smoke tests created

---
+++ src/components/scaffold/Header.tsx
@@ -0,0 +1,12 @@
+// Scaffold component - Header of the page
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      Main
+    </div>
+  )
+};
+
+export default Header;
